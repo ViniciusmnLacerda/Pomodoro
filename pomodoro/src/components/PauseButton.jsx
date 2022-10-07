629c62c7d2302506ce14994316e4580c0ed52f2a
Vinícius Lacerda <viniciuslmn@gmail.com>
2022-10-07 15:15:38 -0300

    fix pause/continue button

--- pomodoro/src/components/PauseButton.jsx
+++ pomodoro/src/components/PauseButton.jsx
@@ -2,14 +2,35 @@ import React, { useContext } from 'react';
 import Context from '../context/Context';
 
 function PauseButton() {
-  const { isPaused, setIsPaused, setControlStopWatch } = useContext(Context);
+  const {
+    isPaused,
+    setIsPaused,
+    setControlStopWatch,
+    workMinutes,
+    workSeconds,
+    setWorkSeconds,
+    setWorkMinutes,
+    breakMinutes,
+    breakSeconds,
+    setBreakSeconds,
+    setBreakMinutes,
+    setTotalTimeWork,
+    setTotalTimeBreak,
+  } = useContext(Context);
 
   const handleClick = () => {
     if (!isPaused) {
+      setBreakSeconds(breakSeconds);
+      setBreakMinutes(breakMinutes);
+      setWorkSeconds(workSeconds);
+      setWorkMinutes(workMinutes);
+      setTotalTimeWork((workMinutes * 60) + workSeconds - workMinutes);
+      setTotalTimeBreak((breakMinutes * 60) + breakSeconds - breakMinutes);
       setIsPaused(true);
       setControlStopWatch('not started');
       for (let i = 0; i < 9999; i += 1) {
         window.clearInterval(i);
+        window.clearTimeout(i);
       }
     } else {
       setIsPaused(false);
